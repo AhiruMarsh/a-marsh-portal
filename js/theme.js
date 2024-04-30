fd58f227390e330b9e30dec34705ee1aa00bf65f
AhiruMarsh <91384455+AhiruMarsh@users.noreply.github.com>
2024-04-30 19:57:56 +0900

    update: コメントの追加と画面初期化時の処理を修正

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,34 @@
-function switchTheme() {
-  const getTheme = () => {
-    const storedTheme    = localStorage.getItem('theme')
-    const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+/*
+    theme.js
+    簡易ダークモード対応
+*/
+
+// 現在のテーマを取得
+function getCurrentTheme() {
+  // 環境設定を取得
+  const storedTheme    = localStorage.getItem('theme')
+
+  // システム設定を取得 (未定義の場合: light)
+  const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     
-    if (storedTheme) {
-      return storedTheme
+  // 優先度設定
+  // システム設定よりも環境設定を優先する
+  if (storedTheme) {
+    return storedTheme
   
-    } else {
-      localStorage.setItem('theme', preferredTheme)
-    }
-    
-    return preferredTheme
+  } else {
+    localStorage.setItem('theme', preferredTheme)
   }
+    
+  return preferredTheme
+}
 
-  const currentTheme = getTheme()
+// テーマ切り替え
+function switchTheme() {
+  // 変更前のテーマ
+  const currentTheme = getCurrentTheme();
 
+  // 変更後のテーマ
   let newTheme
   switch (currentTheme) {
     case 'light':
@@ -28,6 +42,11 @@ function switchTheme() {
       break; 
   }
 
+  // 変更内容を反映し、環境設定に保存
   document.documentElement.setAttribute('data-bs-theme', newTheme)
   localStorage.setItem('theme', newTheme)
 }
+
+// 画面初期化時に、反映
+const initTheme = getCurrentTheme()
+document.documentElement.setAttribute('data-bs-theme', initTheme);
